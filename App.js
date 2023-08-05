cd34de31b444868bc19eba23e2c225545b839888
PaisanKjn <bank200074@gmail.com>
2023-08-05 11:42:58 +0700

    Second Commit [Week6-August-05-2023]

--- App.js
+++ App.js
@@ -1,22 +1,15 @@
 import { StyleSheet, Text, View, TouchableHighlight, TouchableOpacityBase, Image } from "react-native";
 import React from "react";
-import TouchablePractice from "./components/TouchablePractice";
+import UseEffectFlatList from "./components/UseEffectFlatList";
 
 const App = () => {
   return (
-    <View style = {styles.container}>
-      <TouchablePractice/>
+    <View>
+      <UseEffectFlatList/>
     </View>
   );
 };
 
 export default App;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    margin: 10,
-    marginTop: 30,
-    padding: 30,
-  },
-});
+const styles = StyleSheet.create({});
